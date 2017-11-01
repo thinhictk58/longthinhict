f37e52ce7c7c534b90df79d4a2b216a44ba7f1da
Thanh Long <thienlong95z@gmail.com>
2017-11-01 18:51:49 +0700

    fix test

--- server-side-code/upload.php
+++ server-side-code/upload.php
@@ -4,11 +4,11 @@
 $target_path = dirname(__FILE__).'/uploads/';
   
 if (isset($_FILES['image']['name'])) {
-    $target_path = $target_path . basename($_FILES['image']['name']);
+    $target_path = $target_path . basename($_FILES['image']['name']['description']);
  
     try {
         // Throws exception incase file is not being moved
-        if (!move_uploaded_file($_FILES['image']['tmp_name'], $target_path)) {
+        if (!move_uploaded_file($_FILES['image']['tmp_name']['tmp_description'], $target_path)) {
             // make error flag true
             echo json_encode(array('status'=>'fail', 'message'=>'could not move file'));
         }
